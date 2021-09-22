98d1c11d5ff8f8290ad55b1128dfbce69b5e23f7
Name <user@example.com>
2021-09-22 20:54:05 +1000

    Interacting with Webservices

--- pyWebService/pyWebservice.py
+++ pyWebService/pyWebservice.py
@@ -3,47 +3,75 @@ import os
 import requests
 import sys
 import csv
+import time
+start_time = time.time()
+feedback = {}
 
 
 def build_feedback(filename):
-    print(filename)
-#     """Populate a dictionary with name/email pairs for easy lookup."""
-#     feedback = {}
-#     with open(filename) as csvfile:
-#         lines = csv.reader(csvfile, delimiter=',')
-#         for row in lines:
-#             name = str(row[0].lower())
-#             email_dict[name] = row[1]
-# feedback = {"title": "Test",
-#             "name": "Jack I.",
-#             "date": "2021-09-01",
-#             "feedback": "Good time to test  "}
-#     return feedback
+
+    idx = 0
+    with open(filename) as csvfile:
+        lines = csv.reader(csvfile)
+        for row in lines:
+            if idx == 0:
+                idx += 1
+                feedback["title"] = row[0]
+            elif idx == 1:
+                idx += 1
+                feedback["name"] = row[0]
+            elif idx == 2:
+                idx += 1
+                feedback["date"] = row[0]
+            elif idx == 3:
+                idx += 1
+                feedback["feedback"] = row[0]
+
+    return feedback
 
 
 def main():
 
     # path = "/data/feedback"
-    path = "C:\\Users\\jcste\\googleAutomation\\pyWebService\data"
+    path = "C:\\Users\\jcste\\googleAutomation\\pyAutomate\\pyWebService\data\\"
 
     for file in os.listdir(path):
         if not file.startswith('.'):
-            # print(file)
-            build_feedback(file)
-            # response = requests.post(
-            #     'http://34.66.203.174/feedback/', data=feedback)
-            # response.raise_for_status()
-            # print(response.text)
-            # print(response.status_code)
+            build_feedback(path + file)
+            print(feedback)
+            response = requests.post(
+                'http://35.193.87.211/feedback/', data=feedback)
+            response.raise_for_status()
+            print(response.text)
+            print(response.status_code)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 
 if __name__ == "__main__":
     main()
 
-# 1. You should now have a list that contains all of the feedback files from the path /data/feedback. Traverse over each file and, from the contents of these text files,
-# create a dictionary by keeping title, name, date, and feedback as keys for the content value, respectively.
-# 2. Now, you need to have a dictionary with keys and their respective values (content from feedback files). This will be uploaded through the Django REST API.
-# 3. Use the Python requests module to post the dictionary to the company's website. Use the request.post() method to make a POST request to http://<corpweb-external-IP>/feedback.
-#  Replace <corpweb-external-IP> with corpweb's external IP address.
-# Make sure an error message isn't returned. You can print the status_code and text of the response objects to check out what's going on.
-# You can also use the response status_code 201 for created success status response code that indicates the request has succeeded.
+# list_of_files = os.listdir('/data/feedback')
+# for file in list_of_files:
+#         fp = open('/data/feedback/'+file)
+#         data = fp.read()
+#         data = data.split('\n')
+#         dic = {"title":data[0], "name":data[1], "date":data[2], "feedback":data[3]}
+#         response = requests.post('http://104.197.6.205/feedback/', json=dic)
+#         print(response.status_code)
+
+# feedback_dir = "/data/feedback/"
+# website_dir = "/projects/corpweb/"
+# url = "http://34.68.197.162/feedback/"
+# feedback_dic = {}
+
+# for feedback_file in os.listdir(feedback_dir):
+#     with open(feedback_dir + feedback_file,"r") as f:
+#         lines = f.readlines()
+#         feedback_dic["title"] = lines[0]
+#         feedback_dic["name"] = lines[1]
+#         feedback_dic["date"] = lines[2]
+#         feedback_dic["feedback"] = lines[3]
+#         response = requests.post(url, data=feedback_dic)
+#         print(response.status_code)
+#         print(response.ok)
